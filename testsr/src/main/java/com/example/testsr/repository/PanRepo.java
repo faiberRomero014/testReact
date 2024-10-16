1bea68d9369e6308c7769c633c4dc2a3b61103aa
faiberRomero014 <129418859+faiberRomero014@users.noreply.github.com>
2024-10-16 10:03:51 -0500

    a

--- testsr/src/main/java/com/example/testsr/repository/PanRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.testsr.repository;
-
-import com.example.testsr.model.Pan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PanRepo extends JpaRepository<Pan, Integer> {
-}
